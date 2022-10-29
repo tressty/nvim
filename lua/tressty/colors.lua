e1d935755af6da077eeae608654e4ec1ada27d38
Trard <trardone@gmail.com>
2022-10-29 23:41:36 +0300

    mostly done

--- lua/tressty/colors.lua
+++ lua/tressty/colors.lua
@@ -1,59 +1,53 @@
--- This file is used to define
--- the basic colors of the theme
--- and the style specific colors
-
 local colors = {
-	-- Common colors
-	white = "#EEFFFF",
-	gray = "#717CB4",
-	black = "#000000",
-	red = "#F07178",
-	green = "#C3E88D",
-	yellow = "#FFCB6B",
-	blue = "#82AAFF",
-	paleblue = "#B0C9FF",
-	cyan = "#89DDFF",
-	purple = "#C792EA",
-	orange = "#F78C6C",
-	pink = "#FF9CAC",
-
-	-- Dark colors
-	darkred = "#DC6068",
-	darkgreen = "#ABCF76",
-	darkyellow = "#E6B455",
-	darkblue = "#6E98EB",
-	darkcyan = "#71C6E7",
-	darkpurple = "#B480D6",
-	-- darkorange =			'#E2795B',
-
-	error = "#FF5370",
-	link = "#80CBC4",
-	cursor = "#FFCC00",
-	title = "#E2E2E2",
-	-- visual_alt = 			'#3B3D0E',
-
-	none = "NONE",
+	main = {
+		background = "#0F111A",
+		foreground = "#8F93A2",
+		text = "#4B526D",
+		selection_background = "#404767",
+		selection_foreground = "#FFFFFF",
+		buttons = "#191A21",
+		second_background = "#181A1F",
+		disabled = "#464B5D",
+		contrast = "#090B10",
+		active = "#1A1C25",
+		border = "#0F111A",
+		highlight = "#1F2233",
+		tree = "#717CB430",
+		notifications = "#090B10",
+		accent = "#84ffff",
+		excluded = "#292D3E",
+        none = "NONE"
+	},
+	general = {
+		green = "#C3E88D",
+		turquoise = "#80CBC4",
+		blue = "#82AAFF",
+		cyan = "#89DDFF",
+		yellow = "#FFCB6B",
+		orange = "#F78C6C",
+		red = "#F07178",
+		error = "#ff5370",
+		pink = "#FF9CAC",
+		purple = "#C792EA",
+		magenta = "#cc76e9",
+		gray = "#717CB4",
+		white = "#E2E2E2",
+		black = "#090B10",
+	},
+	additional = {
+		moon_light = "#aec8ff",
+		pale_turquoise = "#acddd9",
+		pale_green = "#d8f0b5",
+		sand = "#ffdd9f",
+		cantaloupe = "#fab49f",
+		pale_black = "#171825",
+		vermilion = "#ff5a49",
+        dark_yellow = "#dcac2c"
+	},
+    toxic = {
+		plum = "#9b117b",
+		crimson = "#d10020",
+    }
 }
-
--- Style specific colors
-
-colors.bg = "#0F111A"
-colors.bg_alt = "#090B10"
-colors.fg = "#A6ACCD"
-colors.text = "#717CB4"
-colors.comments = "#464B5D"
-colors.selection = "#1F2233"
-colors.contrast = "#090B10"
-colors.active = "#1A1C25"
-colors.border = "#232637"
-colors.line_numbers = "#3B3F51"
-colors.highlight = "#1F2233"
-colors.disabled = "#464B5D"
-colors.accent = "#84FFFF"
-colors.vsp = colors.border
-
-colors.sidebar = colors.bg
-
-colors.bg_cur = colors.active
 
 return colors
--- lua/tressty/theme/init.lua
+++ lua/tressty/theme/init.lua
@@ -3,6 +3,7 @@ local theme = {}
 theme.main = require("tressty.theme.main")
 theme.editor = require("tressty.theme.editor")
 theme.treeSitter = require("tressty.theme.treesitter")
+theme.diagnostic = require("tressty.theme.diagnostic")
 theme.lsp = require("tressty.theme.lsp")
 
 return theme
