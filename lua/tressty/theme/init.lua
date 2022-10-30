67f099fd27d2864725e469d8b93f4d0728414eed
Trard <trardone@gmail.com>
2022-10-30 22:06:23 +0300

    netrw

--- lua/tressty/theme/init.lua
+++ lua/tressty/theme/init.lua
@@ -5,5 +5,6 @@ theme.editor = require("tressty.theme.editor")
 theme.treeSitter = require("tressty.theme.treesitter")
 theme.diagnostic = require("tressty.theme.diagnostic")
 theme.lsp = require("tressty.theme.lsp")
+theme.netrw = require("tressty.theme.netrw")
 
 return theme
---
+++ lua/tressty/theme/netrw.lua
@@ -0,0 +1,5 @@
+local colors = require("tressty.colors")
+
+return {
+    netrwClassify = { link = "Delimiter" }
+}
