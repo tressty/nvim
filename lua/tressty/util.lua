305f79e52331d7568158dac224c1a60bf4ac19a3
Trard <trardone@gmail.com>
2022-10-30 21:37:51 +0300

    terminal colors

--- lua/tressty/util.lua
+++ lua/tressty/util.lua
@@ -10,29 +10,7 @@ local apply_highlights = function(highlights)
 	end
 end
 
--- see colors in https://github.com/tressty/windows-terminal
-local function loadTerminal()
-	vim.g.terminal_color_0 = colors.black
-	vim.g.terminal_color_1 = colors.darkred
-	vim.g.terminal_color_2 = colors.darkgreen
-	vim.g.terminal_color_3 = colors.darkyellow
-	vim.g.terminal_color_4 = colors.darkblue
-	vim.g.terminal_color_5 = colors.darkpurple
-	vim.g.terminal_color_6 = colors.darkcyan
-	vim.g.terminal_color_7 = colors.white
-	vim.g.terminal_color_8 = colors.disabled
-	vim.g.terminal_color_9 = colors.red
-	vim.g.terminal_color_10 = colors.green
-	vim.g.terminal_color_11 = colors.yellow
-	vim.g.terminal_color_12 = colors.blue
-	vim.g.terminal_color_13 = colors.purple
-	vim.g.terminal_color_14 = colors.cyan
-	vim.g.terminal_color_15 = colors.white
-end
-
--- Load the theme
-function util.load()
-	-- Set the theme environment
+local function prepare_environment()
 	vim.cmd("hi clear")
 
 	if vim.fn.exists("syntax_on") then
@@ -43,19 +21,36 @@ function util.load()
 
 	vim.opt.termguicolors = true
 	vim.g.colors_name = "tressty"
+end
 
-	-- vim.opt.guicursor = "n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20,a:Cursor/Cursor"
+-- see colors in https://github.com/tressty/windows-terminal
+local function loadTerminal()
+	vim.g.terminal_color_0 = colors.general.black
+	vim.g.terminal_color_1 = colors.general.red
+	vim.g.terminal_color_2 = colors.general.green
+	vim.g.terminal_color_3 = colors.general.yellow
+	vim.g.terminal_color_4 = colors.general.blue
+	vim.g.terminal_color_5 = colors.general.purple
+	vim.g.terminal_color_6 = colors.general.cyan
+	vim.g.terminal_color_7 = colors.general.white
 
-	-- local exit_group = vim.api.nvim_create_augroup("tresstyExit", { clear = true })
-	-- vim.api.nvim_create_autocmd("ExitPre", {
-		-- command = "autocmd ExitPre * set guicursor=n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20",
-		-- group = exit_group,
-	-- })
+	vim.g.terminal_color_8 = colors.general.gray
+	vim.g.terminal_color_9 = colors.additional.vermilion
+	vim.g.terminal_color_10 = colors.general.green
+	vim.g.terminal_color_11 = colors.general.yellow
+	vim.g.terminal_color_12 = colors.general.blue
+	vim.g.terminal_color_13 = colors.general.purple
+	vim.g.terminal_color_14 = colors.general.cyan
+	vim.g.terminal_color_15 = colors.general.white
+end
 
-	--TODO loadTerminal()
+-- Load the theme
+function util.load()
+	prepare_environment()
+	loadTerminal()
 
-	for _, highlight in pairs(theme) do
-		apply_highlights(highlight)
+	for _, highlights in pairs(theme) do
+		apply_highlights(highlights)
 	end
 end
 
